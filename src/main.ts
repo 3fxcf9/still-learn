16b1d00b11b4778c7a71133d8a828dc01bb57ae0
h3x-code <117070364+h3x-code@users.noreply.github.com>
2023-01-30 20:14:39 +0100

    Add sass and clean useless components

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
-
 // Import the PocketBase JS library
 import client from "@/pocketbase";
 
