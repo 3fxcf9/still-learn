d06a874926e9d576e092677a6a52caa1fee9c32d
3fxcf9 <123954477+3fxcf9@users.noreply.github.com>
2023-12-01 23:58:52 +0100

    Accessibility improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { currentUser } from '$lib/pocketbase';
+	import { base } from '$app/paths';
 </script>
 
 <h1>Still-Learn</h1>
 
 {#if $currentUser}
-	<a href="dashboard">dashboard</a>
+	<a href="{base}/dashboard">dashboard</a>
 {:else}
 	<a href="login">login</a> <a href="signup">sign up</a>
 {/if}
